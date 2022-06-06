a6b6442292376fe37e0aa3c9073c253510143e0e
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-06 16:25:44 +0800

    Project Setup

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
+ReactDOM.render(<p>This is expensify app</p>,document.getElementById('app'));
