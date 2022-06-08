853418eca348a23dd1c13d6930f896727bcffd0f
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-08 16:05:09 +0800

    Added Subscribe and enable dynamic action(playground)

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,27 +2,39 @@ import {createStore} from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch(action.type) {
         case 'INCREEMENT':
+            const incrementBy = typeof action.incrementBy ==="number"? action.incrementBy:1
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
     
             };
         case 'DECREEMENT':
+            const decrementBy = typeof action.decrementBy ==="number"?action.decrementBy:1
             return{
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return{
                 count: 0
+            }
+        case 'SET':
+            return{
+                count: action.count
             }
         default:
             return state;
     }   
 }
 );
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+}
+
+);
+
 store.dispatch(
     {
-        type: 'INCREEMENT'
+        type: 'INCREEMENT',
+        incrementBy: 5
     }
 );
 store.dispatch(
@@ -36,10 +48,22 @@ store.dispatch(
     }
 );
 
-console.log(store.getState());
 store.dispatch(
     {
         type: 'RESET'
     }
 );
-console.log(store.getState());
+store.dispatch(
+    {
+        type: 'DECREEMENT',
+        decrementBy: 10
+
+    }
+);
+store.dispatch(
+    {
+        type: 'SET',
+        count: 101
+
+    }
+);
