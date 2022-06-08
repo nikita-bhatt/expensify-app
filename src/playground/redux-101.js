28cf45b8422ca07877f16b61fcda2b309f3d5b50
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-08 14:13:26 +0800

    Dispatching Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,45 @@
 import {createStore} from 'redux';
-const store = createStore((state = {count: 0}) => {
-    return state
+const store = createStore((state = {count: 0}, action) => {
+    switch(action.type) {
+        case 'INCREEMENT':
+            return {
+                count: state.count + 1
+    
+            };
+        case 'DECREEMENT':
+            return{
+                count: state.count - 1
+            };
+        case 'RESET':
+            return{
+                count: 0
+            }
+        default:
+            return state;
+    }   
 }
 );
+console.log(store.getState());
+store.dispatch(
+    {
+        type: 'INCREEMENT'
+    }
+);
+store.dispatch(
+    {
+        type: 'DECREEMENT'
+    }
+);
+store.dispatch(
+    {
+        type: 'DECREEMENT'
+    }
+);
 
+console.log(store.getState());
+store.dispatch(
+    {
+        type: 'RESET'
+    }
+);
 console.log(store.getState());
