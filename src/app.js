cee05b29a0e4eced5f1955b454dc4d5aa9250de5
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-13 15:20:15 +0800

    Added actions,reducers,selectors,store for expensify app(app.js)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import 'normalize.css/normalize.css'
-import './styles/style.scss'
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill', amount: 1000, createdAt: 21000}));
+store.dispatch(addExpense({description: 'Gas Bill', amount: 5000, createdAt: 1000}));
+store.dispatch(setTextFilter('Water'));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter/>,document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('styles.css');
   console.log('env', env);
   return {
-    entry: './src/playground/redux-expensify.js',
+    entry: './src/app.js',
     output: {
      path: path.join(__dirname,'public'),
      filename: 'bundle.js'
