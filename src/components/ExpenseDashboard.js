bdfc6dd72897f0b924531a0df1bb3c7bd26f41be
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-14 12:38:22 +0800

    Rendering individual expenses

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExenseListItem from './ExpenseListItem'
+import ExpenseListItem from './ExpenseListItem';
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseList/>
