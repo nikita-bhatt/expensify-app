f5ba86010256e6387c430bb9dddf25ce5f26674a
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-15 12:26:55 +0800

    Added Dropdown for sort By date or amount

--- src/app.js
+++ src/app.js
@@ -11,12 +11,10 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 1000, createdAt: 21000}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 1000}));
 store.dispatch(addExpense({description: 'Gas Bill', amount: 5000, createdAt: 1000}));
-store.dispatch(setTextFilter('Water'));
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000)
+store.dispatch(addExpense({description: 'Rent', amount: 45000}));
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
 console.log(visibleExpenses);
