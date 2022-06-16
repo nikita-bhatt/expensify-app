c7b22059b42f70eed5392f10c27ee84149813ea0
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-16 14:35:28 +0800

    Created Add expense dashboard

--- src/components/AddDashboard.js
+++ src/components/AddDashboard.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from "react-redux";
 import ExpenseForm from './ExpenseForm';
-const AddDashboardPage = () => (
+import {addExpense} from '../actions/expenses';
+const AddDashboardPage = (props) => (
     <div>
-        This is Add dashboard component
-        <ExpenseForm/>
+        Add Expense
+        <ExpenseForm
+        onSubmit= {(expense) => {
+            props.dispatch(addExpense(expense));
+            props.history.push('/');
+        }}
+        />
     </div>
 
 );
 
-export default AddDashboardPage;
+export default connect()(AddDashboardPage);
