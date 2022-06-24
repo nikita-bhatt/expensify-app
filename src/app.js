994ca3ff36336b1d393ed60c4d5e55c9f92836f4
Nikita Bhatt <nikitabhattpth@gmail.com>
2022-06-24 12:42:42 +0800

    Removed Dummy Expenses

--- src/app.js
+++ src/app.js
@@ -5,19 +5,9 @@ import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 1000}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 5000, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 45000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
 const jsx = (
     <Provider store = {store}>
         <AppRouter/>
